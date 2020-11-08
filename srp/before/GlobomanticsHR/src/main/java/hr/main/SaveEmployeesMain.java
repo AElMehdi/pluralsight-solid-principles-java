9c5e5d2b2d5c5f158b714b8e880415b78a052384
AElMehdi <assirelmehdi@gmail.com>
2020-11-08 20:51:18 +0100

    Single Responsability principle solution

--- srp/before/GlobomanticsHR/src/main/java/hr/main/SaveEmployeesMain.java
+++ srp/before/GlobomanticsHR/src/main/java/hr/main/SaveEmployeesMain.java
@@ -1,19 +1,30 @@
 package hr.main;
 
+import hr.logging.ConsoleLogger;
 import hr.persistence.EmployeeRepository;
 import hr.personnel.Employee;
+import hr.serializers.EmployeeFileSerializer;
 
+import java.io.IOException;
 import java.util.List;
 
 public class SaveEmployeesMain {
     public static void main(String[] args) {
         // Grab employees
-        EmployeeRepository repository = new EmployeeRepository();
+        EmployeeFileSerializer employeeFileSerializer = new EmployeeFileSerializer();
+        ConsoleLogger consoleLogger = new ConsoleLogger();
+
+        EmployeeRepository repository = new EmployeeRepository(employeeFileSerializer);
         List<Employee> employees = repository.findAll();
 
         // Save all
         for (Employee e : employees){
-            Employee.save(e);
+            try {
+                repository.save(e);
+                consoleLogger.logInfo(e.toString());
+            } catch (IOException ioException) {
+                consoleLogger.logError(ioException);
+            }
         }
     }
 }
--- srp/before/GlobomanticsHR/src/main/java/hr/personnel/Employee.java
+++ srp/before/GlobomanticsHR/src/main/java/hr/personnel/Employee.java
@@ -1,10 +1,5 @@
 package hr.personnel;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 /*
 Models an employee form a business perspective
  */
@@ -20,6 +15,14 @@ public abstract class Employee {
         String[] names = fullName.split(" ");
         this.firstName = names[0];
         this.lastName = names[1];
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 
     public String getEmail() {
@@ -63,31 +66,4 @@ public abstract class Employee {
         return this.firstName + " " + this.lastName;
     }
 
-    public static void save(Employee employee){
-        try {
-            StringBuilder sb = new StringBuilder();
-            sb.append("### EMPLOYEE RECORD ####");
-            sb.append(System.lineSeparator());
-            sb.append("NAME: ");
-            sb.append(employee.firstName + " " + employee.lastName);
-            sb.append(System.lineSeparator());
-            sb.append("POSITION: ");
-            sb.append(employee.getClass().getTypeName());
-            sb.append(System.lineSeparator());
-            sb.append("EMAIL: ");
-            sb.append(employee.getEmail());
-            sb.append(System.lineSeparator());
-            sb.append("MONTHLY WAGE: ");
-            sb.append(employee.monthlyIncome);
-            sb.append(System.lineSeparator());
-
-            Path path = Paths.get(employee.getFullName()
-                    .replace(" ","_") + ".rec");
-            Files.write(path, sb.toString().getBytes());
-
-            System.out.println("Saved employee " + employee.toString());
-        } catch (IOException e){
-            System.out.println("ERROR: Could not save employee. " + e);
-        }
-    }
 }
